f20730e444e8050e4a93535e7f1440e2b312953b
Marcus Bynoe <marcusbynoe57@gmail.com>
2022-12-18 20:38:43 -0700

    solved prob 4

--- js/app.js
+++ js/app.js
@@ -81,12 +81,14 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-  var add4 = sum()
+  var add4 = sum(sum(sumArr[0], sumArr[1])[0], (sumArr[2]))[0];
+  let prob4 = `${sumArr} was passed in as an array of numbers, and 9 is their sum.`;
+  return[add4, prob4];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
